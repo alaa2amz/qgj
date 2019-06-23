8495cb7ab1089a9d51c30f526e7876006a0fb487
alaa 2 amz <alaa2amz@gmail.com>
2019-06-23 13:58:23 +0200

    This is a country where people are free to practice their religion, regardless of race, creed, color, obesity, or number of dangling keys...--2019-06-23--work_day_end--

---
+++ krad-parser.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+'''module for parsing kanji krad file and populating its contents into sqlite db file
+
+1st argument krad file
+2nd argument sqlite db file
+
+alaa amz
+Sun Jun 23 13:27:02 EET 2019
+Truth has no special time of its own.  Its hour is now -- always.
+		-- Albert Schweitzer
+
+'''
+
+import sys
+import sqlite3
+
+def insert_ignore_select(table_name,value):
+    '''insert value or ignore if existed and select its id for a lookup single collummed table
+
+    parametrs:-
+    table_name: the name of the table
+    valu: the value to insert and lookup
+    note:by default the value is inserted as for {table_name}.{table_name}_value
+    todo: adding fields and corresponding values tuples
+    '''
+    
+    insert_ignore_query_string = f'INSERT OR IGNORE INTO {table_name}({table_name}_value) VALUES (?)'
+    select_query = f'SELECT {table_name}_id from {table_name} where {table_name}_value = ?'
+    cur.execute(insert_ignore_query_string,(value,))
+    result = cur.execute(select_query,(value,))
+    id = result.fetchone()[0]
+    return id
+
+
+con = sqlite3.connect()
+cur = con.cursor()
+kradfile = sys.argv[1]
+db_file = sys.argv[2]
+
+def parse(kradfile=kradfile,db_file=db_file):
+    file = open(kradfile,'r')
+    line = file.readline()
+    while line != '':
+        if line[0] == '#':
+            line = readline
+        else:
+            literal = line[0]
+            parts = line[4:].strip().split()
+            literal_id = insert_ignore_select('literal',literal)
+            
+            for part in parts:
+                part_id = insert_ignore_select('parts',part)
+                q = 'insert into k_parts (literal_id,parts_id) values (?,?)'
+                cur.execute(q,(literal_id,part_id))
+    con.commit()
+    con.close()
+    print('done')
+
+if __name__ = '__main__':
+    parse()
