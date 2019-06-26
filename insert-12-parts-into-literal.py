753f6455a792fc4eb97d6c3f9cd3389f3a5690dd
alaa <alaa2amz@gmail.com>
2019-06-26 05:35:41 +0200

    Alimony is a system by which, when two people make a mistake, one of them continues to pay for it. 		-- Peggy Joyce

---
+++ insert-12-parts-into-literal.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+
+import sys
+import sqlite3
+db_file = sys.argv[1]
+con = sqlite3.connect(db_file)
+cur = con.cursor()
+
+parts = '''\
+ノ KATAKANA LETTER NO
+ヨ KATAKANA LETTER YO
+｜ FULLWIDTH VERTICAL LINE
+ハ KATAKANA LETTER HA
+⺅ CJK RADICAL PERSON
+⻖ CJK RADICAL MOUND TWO
+⺾ CJK RADICAL GRASS ONE
+⺌ CJK RADICAL SMALL ONE
+⺹ CJK RADICAL OLD
+⻏ CJK RADICAL CITY
+マ KATAKANA LETTER MA
+ユ KATAKANA LETTER YU
+'''
+
+def insert_ignore_select(table_name,value):
+    insert_ignore_query_string = f'INSERT OR IGNORE INTO {table_name}({table_name}_value) VALUES (?)'
+    select_query = f'SELECT {table_name}_id from {table_name} where {table_name}_value = ?'
+    cur.execute(insert_ignore_query_string,(value,))
+    result = cur.execute(select_query,(value,))
+    id = result.fetchone()[0]
+    return id
+
+def insert_12_parts_literals(parts=parts,db_file=db_file):
+    
+
+    parts=parts.lower()
+    parts_lines=parts.splitlines()
+
+    for line in parts_lines:
+        part_list = line.split(maxsplit=1)
+        id = insert_ignore_select('literal',part_list[0])
+        q = 'insert into meaning (literal_id,meaning_value) values (?,?)'
+        cur.execute(q,(id,part_list[-1]))
+        print(id,part_list)
+    con.commit()
+    con.close()
+    print('done')
+
+
+if __name__ == '__main__':
+    insert_12_parts_literals()
---
+++ replace-holders.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+
+place_holders ='''\
+化 ⺅
+刈 ⺉
+込 ⻌
+汁 氵
+初 衤
+尚 ⺌
+買 罒
+犯 犭
+忙 忄
+礼 礻
+个 𠆢
+老 ⺹
+扎 扌
+杰 灬
+疔 疒
+禹 禸
+艾 ⺾
+邦 ⻏
+阡 ⻖
+'''
+
+import sys
+import sqlite3
+db_file = sys.argv[1]
+con = sqlite3.connect(db_file)
+cur = con.cursor()
+
+def insert_ignore_select(table_name,value):
+    insert_ignore_query_string = f'INSERT OR IGNORE INTO {table_name}({table_name}_value) VALUES (?)'
+    select_query = f'SELECT {table_name}_id from {table_name} where {table_name}_value = ?'
+    cur.execute(insert_ignore_query_string,(value,))
+    result = cur.execute(select_query,(value,))
+    id = result.fetchone()[0]
+    return id
+
+def replace_holders(place_holders=place_holders, db_file=db_file):
+    place_holders = place_holders.splitlines()
+    for line in place_holders:
+        list = line.split()
+        id = insert_ignore_select('parts',list[0])
+        q = 'update  parts set parts_value = ? where parts_id= ?'
+        cur.execute(q,(list[1],id))
+        print(id,list,sep='-->')
+    con.commit()
+    con.close()
+    #print('done')
+
+if __name__ == '__main__':
+    replace_holders()
