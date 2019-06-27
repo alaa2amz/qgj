d3e10494e9e10a88dd3763f07c2811c59d53eaaa
alaa 2 amz <alaa2amz@gmail.com>
2019-06-27 11:08:38 +0200

    The Guy on the Right Doesn't Stand a Chance The guy on the right has the Osborne 1, a fully functional computer system in a portable package the size of a briefcase.  The guy on the left has an Uzi submachine gun concealed in his attache case.  Also in the case are four fully loaded, 32-round clips of 125-grain 9mm ammunition.  The owner of the Uzi is going to get more tactical firepower delivered -- and delivered on target -- in less time, and with less effort.  All for $795. It's inevitable. If you're going up against some guy with an Osborne 1 -- or any personal computer -- he's the one who's in trouble.  One round from an Uzi can zip through ten inches of solid pine wood, so you can imagine what it will do to structural foam acrylic and sheet aluminum.  In fact, detachable magazines for the Uzi are available in 25-, 32-, and 40-round capacities, so you can take out an entire office full of Apple II or IBM Personal Computers tied into Ethernet or other local-area networks.  What about the new 16-bit computers, like the Lisa and Fortune?  Even with the Winchester backup, they're no match for the Uzi.  One quick burst and they'll find out what Unix means.  Make your commanding officer proud.  Get an Uzi -- and come home a winner in the fight for office automatic weapons. 		-- "InfoWorld", June, 1984--2019-06-27--work_day_end--regex working -- tuples to list todo history and time recording insid and outside in python and shell

--- qgj.py
+++ qgj.py
@@ -5,18 +5,27 @@ import sqlite3
 db_name = 'kd3.db'
 con = sqlite3.connect(db_name)
 cur = con.cursor()
+
+def _regexp(pattern,input_string):
+    import re
+    pa = result = re.compile(pattern)
+    return pa.search(input_string) is not None
+    
+con.create_function('REGEXP',2,_regexp)
 def wamk(args=sys.argv):
     '''where are my kanjies'''
-    q = 'select literal.literal_value from kmlp,literal,k_parts where kmlp.pis = k_parts.parts_id and k_parts.literal_id = literal.literal_id and kmlp.meaning = ?'
+    q = 'select literal.literal_value from kmlp,literal,k_parts where kmlp.pis = k_parts.parts_id and k_parts.literal_id = literal.literal_id and kmlp.meaning REGEXP ?'
     kanji_set = set()
     for arg in args:
+        arg=r'\b'+arg+r'\b'
         ro=cur.execute(q,(arg,))
         ro=set(ro)
         if len(ro) == 0 or len(kanji_set) == 0:
             kanji_set = kanji_set.union(ro)
         else:
             kanji_set = kanji_set.intersection(ro)
-    print(kanji_set)
+            kanji_list = [x[0] for x in kanji_set]
+    print(kanji_list)
 
 if __name__ == '__main__':
     wamk()
