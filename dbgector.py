b6452ff23190d45b79c49510f9f1f0a35b6a69bc
alaa <alaa2amz@gmail.com>
2019-06-30 08:43:12 +0200

    The average individual's position in any hierarchy is a lot like pulling a dogsled -- there's no real change of scenery except for the lead dog.

--- dbgector.py
+++ dbgector.py
@@ -23,7 +23,15 @@ def dbgect(field_id,value):
     results = {}
     for table_name in tables_names:
         if field_id in table_fields_dict[table_name]:
-            q = f'select * from {table_name} where {field_id} = ?'
+            from_list=[table_name]
+            where_dict[field_id]=value#####
+            for field in table_fields_dict[table_name]:
+                if field[-3:]=='_id' and field != field_id\
+                and field[:-3] in tables_names:
+                    from_list.append(field[:-3])
+                    where_dict[table_name+'.'+field]=field[:-3]+'.'+field###
+            where_list = [
+            q = f'select * from {"."join(from_list)} where {} = ?'
             print(table_name,field_id,value,'7777')
             r=cur.execute(q,(value,))
             results[table_name]=[tuple(table_fields_dict[table_name])]+r.fetchall()
